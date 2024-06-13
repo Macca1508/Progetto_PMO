68a235ac5ec133573af24339627c188501a40e7f
Macca1508 <t.maccaroni@campus.uniurb.it>
2024-06-13 16:43:07 +0200

    remove

--- Desktop/ProgettoPMO/src/model/piece/Piece.java
+++
@@ -1,6 +0,0 @@
-package model.piece;
-
-public interface Piece {
-	public void moveForwards();
-	public void moveBackwards();
-}
--- Desktop/ProgettoPMO/src/model/slot/ActionSlot.java
+++
@@ -1,8 +0,0 @@
-package model.slot;
-import model.piece.PieceImp;
-
-public interface ActionSlot{
-	void action(PieceImp piece);
-	int getSlotName();
-	String message();
-}
